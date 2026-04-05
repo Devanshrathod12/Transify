3c12b928a597645656e0a68321a6d9d77f3148dd
DevanshRathod12 <rathoddevansh53@gmail.com>
2026-04-05 09:06:34 +0530

    commit

--- src/i18n/i18n.jsx
+++ src/i18n/i18n.jsx
@@ -14,19 +14,3 @@ i18n.fallbacks = true;
 i18n.locale = "en";
 
 export default i18n;
-// import i18n from "i18n-js";
-
-// import en from "../i18n/translations/en.json";
-// import hi from "../i18n/translations/hi.json";
-// import te from "../i18n/translations/te.json";
-
-// i18n.translations = {
-//   en,
-//   hi,
-//   te,
-// };
-
-// i18n.fallbacks = true;
-// i18n.locale = "en";
-
-// export default i18n;
